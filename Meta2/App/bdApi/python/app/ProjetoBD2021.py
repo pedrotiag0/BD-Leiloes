5015169b61ea5018d6eaca14115739d659388d6f
DJordao <diogojordao.2504@gmail.com>
2021-05-26 19:08:58 +0100

    Merge branch 'main' of https://github.com/pedrotiag0/BD-Leiloes into main

--- Meta2/App/bdApi/python/app/ProjetoBD2021.py
+++ Meta2/App/bdApi/python/app/ProjetoBD2021.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify, request
-import logging, psycopg2, time
+import logging, psycopg2, time, jwt, datetime
 
 app = Flask(__name__)
-
+KEY = "b96ZhIxcBdxNPDn4WRzDueMMqyux3k7g"
 
 @app.route('/')
 def hello():
@@ -27,7 +27,6 @@ def hello():
 ##
 ##
 
-
 @app.route("/dbproj/user", methods=['POST'])
 def addUser():
     logger.info("###              BD [Insert User]: POST /dbproj/user              ###");
@@ -44,22 +43,62 @@ def addUser():
     # parameterized queries, good for security and performance
     statement = """
                   INSERT INTO utilizador (username, email, password) 
-                          VALUES ( %s,   %s ,   %s )"""
+                          VALUES ( %s,   %s ,   %s ) RETURNING userid"""
 
-    values = (payload["username"], payload["email"], payload["password"])
+    try:
+        values = (payload["username"], payload["email"], payload["password"])
+    except (Exception) as error:
+        codigoErro = '003'  # Payload incorreto (nome das variaveis)
+        return jsonify(erro=codigoErro)
+
+    if not ((32>=len(values[0])>0) and (64>=len(values[1])>0) and (32>=len(values[2])>0)):
+        codigoErro = '002'  # Input Invalido
+        return jsonify(erro=codigoErro)
 
     try:
         cur.execute(statement, values)
+        sucess = True
+        novoUserId = str(cur.fetchone()[0])
         cur.execute("commit")
-        result = 'Inserted!'    # Tem de ser alterado
+    except psycopg2.IntegrityError:
+        sucess = False
+        codigoErro = '001'  # Utilizador duplicado
     except (Exception, psycopg2.DatabaseError) as error:
         logger.error(error)
-        result = 'Failed!'      # Tem de ser alterado
+        sucess = False
+        codigoErro = '999'  # Erro nao identificado
     finally:
         if conn is not None:
             conn.close()
+    if(sucess):
+        return jsonify(userId=novoUserId)
+    else:
+        return jsonify(erro=codigoErro)
 
-    return jsonify(result)
+@app.route("/dbproj/leiloes", methods=['GET'], strict_slashes=True)
+def get_all_auctions():
+    logger.info("###              BD [LIST AUCTIONS]: GET /dbproj/leiloes              ###")
+
+    conn = db_connection()
+    cur = conn.cursor()
+
+    try:
+        cur.execute("SELECT leilaoid, descricao FROM leilao WHERE datafim > (NOW() + INTERVAL '1 hours')")
+    except (Exception, psycopg2.DatabaseError) as error:
+        logger.error(error)
+        return jsonify(erro='999')
+
+    rows = cur.fetchall()
+
+    payload = []
+    logger.debug("---- Leiloes  ----")
+    for row in rows:
+        logger.debug(row)
+        content = {'leilaoId': int(row[0]), 'descricao': row[1]}
+        payload.append(content)                                                                     # appending to the payload to be returned
+
+    conn.close()
+    return jsonify(payload)
 
 def db_connection():
     db = psycopg2.connect(user = "aulaspl",
